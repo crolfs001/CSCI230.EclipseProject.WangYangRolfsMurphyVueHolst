4cb72a9489c3880058358e5b31859855ac716980
CROLFS001 <crolfs001@vplincsci049.ad.csbsju.edu>
2020-02-23 14:37:31 -0600

    first commit

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package edu.csbsju.cs;
+
+/**
+ * @author dmurphy001
+ *
+ */
+public class SimplePrint {
+	private String pName;
+	private int pAge;
+	/**
+	 * 
+	 */
+	public SimplePrint() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Devin Murphy\nFue Vue");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("args[ " + i + " ] = " + args[i]);
+		}
+	}
+
+	/**
+	 * @return the pName
+	 */
+	public String getpName() {
+		return pName;
+	}
+
+	/**
+	 * @param pName the pName to set
+	 */
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	/**
+	 * @return the pAge
+	 */
+	public int getpAge() {
+		return pAge;
+	}
+
+	/**
+	 * @param pAge the pAge to set
+	 */
+	public void setpAge(int pAge) {
+		this.pAge = pAge;
+	}
+
+}
